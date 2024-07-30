59be60b89a9371aa89a530ae09894a8a569154ce
vibsthebot <142349522+vibsthebot@users.noreply.github.com>
2024-07-30 16:20:01 -0700

    Update Board.java to add neighbor and twin functionality (still needs fixing)

--- Board.java
+++ Board.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import static java.lang.Math.abs;
 
 public class Board {
@@ -11,17 +15,17 @@ public class Board {
     }
     // string representation of this board
     public String toString() {
-        String boardAsString = "";
-        boardAsString = boardAsString + width + "\n";
+        StringBuilder boardAsString = new StringBuilder();
+        boardAsString.append(width).append("\n");
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < width; j++) {
-                boardAsString = boardAsString + " " + board[i][j];
+                boardAsString.append(" ").append(board[i][j]);
             }
-            boardAsString = boardAsString + "\n";
+            boardAsString.append("\n");
         }
 
 
-        return boardAsString;
+        return boardAsString.toString();
     }
 
     // board dimension n
@@ -81,32 +85,82 @@ public class Board {
     // does this board equal y?
     public boolean equals(Object y) {
         if (y == this) return true;
-        if (y == null) return false;
-        if (y.getClass() != this.getClass()) return false;
-        Board that = (Board) y;
-        if (that.dimension() != this.dimension())
-            return false;
-        for (int i = 0; i < board.length; i ++) {
-            if (this.board[i] != that.board[i])
-                return false;
+        if (y == null || y.getClass() != getClass()) return false;
+        Board other = (Board) y;
+        if (other.dimension() != width) return false;
+        for (int i = 0; i < width; i++){
+            for (int j = 0; j < width; j++){
+                if (other.board[i][j] != board[i][j]) return false;
+            }
         }
         return true;
     }
 
-    /*
+
     // all neighboring boards
     public Iterable<Board> neighbors() {
+        boolean found = false;
+        int i = 0, j = 0;
+        while (i < width) {
+            while (j < width) {
+                if (board[i][j] == 0) {
+                    found = true;
+                    break;
+                }
+                j++;
+            }
+            if (found) break;
+            i++;
+        }
+
+        List<Board> boardList = new ArrayList<Board>();
+
+        if (i > 0) {
+            boardList.add(elementSwap(i, j, i - 1, j));
+        }
+        if (i < width - 1) {
+            boardList.add(elementSwap(i, j, i + 1, j));
+        }
+        if (j > 0) {
+            boardList.add(elementSwap(i, j, i, j - 1));
+        }
+        if (j < width - 1) {
+            boardList.add(elementSwap(i, j, i, j + 1));
+        }
+
+        return boardList;
 
     }
 
     // a board that is obtained by exchanging any pair of tiles
     public Board twin() {
 
+        Random random = new Random();
+
+        // Generate two random positions
+        int row1 = random.nextInt(width);
+        int col1 = random.nextInt(width);
+        int row2 = random.nextInt(width);
+        int col2 = random.nextInt(width);
+
+        // Swap the elements
+
+        return elementSwap(row1, col1, row2, col2);
     }
-    */
+
+    public Board elementSwap(int row1, int col1, int row2, int col2) {
+        int[][] returnMatrix = new int[width][width];
+        Board returnBoard = new Board(board);
+        returnBoard.board[row1][col1] = board[row2][col2];
+        returnBoard.board[row2][col2] = board[row1][col1];
+        return returnBoard;
+    }
+
     public static void main(String[] args) {
-        Board tiles  = new Board(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 0}});
-        Board tiles2  = new Board(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 0}});
+        Board tiles  = new Board(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+        Board tiles2  = new Board(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+        System.out.println(tiles.equals(tiles2));
+        System.out.println(tiles.twin());
         System.out.println(tiles);
     }
 
